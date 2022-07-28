0705be6acc583e955c60dbbd971455c0267d2ad0
Vicente Jose Santiago Costa Oliveira <vicente.oliveira@HANHELP-33332.local>
2022-07-28 11:33:38 -0300

    ensure RemoteAuthentication throws UnexpectedError on 200 with invalid data

--- lib/data/models/remote_account_model.dart
+++ lib/data/models/remote_account_model.dart
@@ -1,3 +1,4 @@
+import '../http/http.dart';
 import '../../domain/entities/entities.dart';
 
 class RemoteAccountModel {
@@ -5,8 +6,12 @@ class RemoteAccountModel {
 
   RemoteAccountModel(this.accessToken);
 
-  factory RemoteAccountModel.fromJson(Map json) =>
-      RemoteAccountModel(json['accessToken']);
+  factory RemoteAccountModel.fromJson(Map json) {
+    if (!json.containsKey('accessToken')) {
+      throw HttpError.invalidData;
+    }
+    return RemoteAccountModel(json['accessToken']);
+  }
 
   AccountEntity toEntity() => AccountEntity(accessToken);
 }

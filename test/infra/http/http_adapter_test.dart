d758eea38e0d4a5a1ff15c5054290154c1227bf4
Vicente Jose Santiago Costa Oliveira <vicente.oliveira@HANHELP-33332.local>
2022-07-31 21:24:45 -0300

    ensure HttpAdapter calls post without body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -16,7 +16,8 @@ class HttpAdapter {
       "content-type": "application;json",
       "accept": "application/json"
     };
-    await client.post(url, headers: headers, body: jsonEncode(body));
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    await client.post(url, headers: headers, body: jsonBody);
   }
 }
 
@@ -43,6 +44,15 @@ void main() {
             "accept": "application/json"
           },
           body: '{"any_key":"any_value"}'));
+    });
+
+    test('Should call post without body', () async {
+      await sut.request(url: url, method: 'post');
+
+      verify(client.post(
+        any,
+        headers: anyNamed('headers'),
+      ));
     });
   });
 }

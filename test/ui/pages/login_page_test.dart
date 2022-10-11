6e43a5099d2f04ad9c6718c78cb0e6e58f802de9
Vicente Jose Santiago Costa Oliveira <vicente.oliveira@HANHELP-33332.local>
2022-10-10 22:47:24 -0300

    ensure loginPage loads with correct initial state

---
+++ test/ui/pages/login_page_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tdd_clean_architecture/ui/pages/pages.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets("Should load with initial state", (WidgetTester tester) async {
+    final loginPage = MaterialApp(home: LoginPage());
+    await tester.pumpWidget(loginPage);
+
+    final emailTextChildren = find.descendant(
+        of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
+    expect(emailTextChildren, findsOneWidget);
+
+    final passwordTextChildren = find.descendant(
+        of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
+    expect(passwordTextChildren, findsOneWidget);
+
+    final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
+    expect(button.onPressed, null);
+  });
+}
